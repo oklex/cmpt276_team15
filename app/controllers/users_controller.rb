3b2360a3e5ab7a152cc7d1ca9e9de21db3ad9e58
Thu Dinh <thudinhj@gmail.com>
2017-11-18 01:51:55 -0800

    add edit user and index users for admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,16 @@ class UsersController < ApplicationController
   before_action :save_login_state, :only => [:new, :create]
   before_action :authenticate_user, :only => [:profile, :settings]
 
+  #GET /users
+  def index
+    @users = User.all
+  end
+
+  #GET/users/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
   # GET /users/new
   def new
     @user = User.new
@@ -17,6 +27,25 @@ class UsersController < ApplicationController
     else
       flash[:notice] = "Form is invalid"
       render "new"
+    end
+  end
+
+  #PATCH/PUT /users/1
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html {redirect_to @user, notice: 'User was successfully updated.'}
+      else
+        format.html {render :edit}
+      end
+    end
+  end
+
+  #DELETE/users/1
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed'}
     end
   end
 
